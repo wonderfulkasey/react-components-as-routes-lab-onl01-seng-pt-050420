bc50de2b5e0918fd87bb7f374aa23d2ab8900465
Kasey Arnold <kaseyarnold15@gmail.com>
2021-02-08 18:29:03 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,27 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div>
-      
+      <div className="navbar">
+      <NavLink
+        to="/"
+        exact
+        activeStyle={{ background: 'lightblue' }}
+      >Home</NavLink>
+      <NavLink
+        to="/movies"
+        exact
+        activeStyle={{ background: 'lightblue' }}
+      >Movies</NavLink>
+      <NavLink
+        to="/directors"
+        exact
+        activeStyle={{ background: 'lightblue' }}
+      >Directors</NavLink>
+      <NavLink
+        to="/actors"
+        exact
+        activeStyle={{ background: 'lightblue' }}
+      >Actors</NavLink>
     </div>
   );
 };
