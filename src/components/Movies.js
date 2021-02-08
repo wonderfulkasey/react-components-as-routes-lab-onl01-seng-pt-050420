6e04b4aafa0259e843e8f3bc517fe0c6fc9e7ea5
Kasey Arnold <kaseyarnold15@gmail.com>
2021-02-08 18:29:37 +0000

    Automatically backed up by Learn

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,24 +1,3 @@
-import React from 'react';
-import { movies } from '../data';
 
-const Movies = () => {
-  return (
-    <div>
-        <h1>Movies Page</h1>
-      {movies.map((movie, index) => (
-        <div key={index}>
-          <h3>{movie.title}</h3>
-          <p>Start time:{movie.time}</p>
-          <p>Genres</p>
-          <ul>
-            {movie.genres.map((genre, index) => (
-              <li key={index}>{genre}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </div>
-  );
-};
 
 export default Movies;
